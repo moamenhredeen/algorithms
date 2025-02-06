f3548fa364500dcb6c5c827efe22f7bfe6af7246
Moamen Hraden <moamen.hraden@primion.eu>
2025-02-06 11:26:31 +0100

    fix file name (it's insertion sort not merge sort)

--- src/insertion_sort.rs
+++ src/insertion_sort.rs
@@ -1,8 +1,7 @@
 use std::vec::Vec;
-use rand::Rng;
 
 /// merge sort
-pub fn merge_sort(arr: &[i32]) -> Vec<i32> {
+pub fn sort(arr: &[i32]) -> Vec<i32> {
 
     if arr.is_empty() || arr.len() == 1 {
         return arr.to_vec();
@@ -26,14 +25,13 @@ pub fn merge_sort(arr: &[i32]) -> Vec<i32> {
 
 #[cfg(test)]
 mod test {
-     use rand::{rngs::mock::StepRng, Rng};
 
-    use super::merge_sort;
+    use super::sort;
 
     #[test]
     fn sort_empty_collection() {
         let arr = vec![];
-        let sorted = merge_sort(&arr);
+        let sorted = sort(&arr);
         assert_eq!(sorted, vec![]);
     }
 
@@ -41,24 +39,16 @@ mod test {
     #[test]
     fn sort_one_element_collection() {
         let arr = vec![1];
-        let sorted = merge_sort(&arr);
+        let sorted = sort(&arr);
         assert_eq!(sorted, vec![1]);
     }
 
 
     #[test]
     fn sort_multiple_item_collection() {
-        let arr = vec![2, 1, 3];
-        let sorted = merge_sort(&arr);
-        assert_eq!(sorted, vec![1, 2, 3]);
-    }
-
-
-    #[test]
-    fn sort_large_collection() {
-        let arr: [i32; 1000] = rand::rng().random();
-        let sorted = merge_sort(&arr);
-        assert!(sorted.is_sorted());
+        let arr = vec![2, 1, 3, 7, 6, 4, 5, 9, 8, 0];
+        let sorted = sort(&arr);
+        assert_eq!(sorted, vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,2 @@
 
-pub mod merge_sort;
+pub mod insertion_sort;
