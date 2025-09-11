c73f6c25ef60981ff6f80ea455bec3b500cf0587
Moamen Hraden <moamen.hraden@primion.eu>
2025-09-11 02:00:46 +0200

    leetcode: longest substring with repeating characters (update)

--- src/leetcode/length_of_longest_substring.c
+++ src/leetcode/length_of_longest_substring.c
@@ -34,32 +34,18 @@ int exists(char c) {
     return -1;
 }
 
-void remove_item(char c, int index) {
-    item_t *item;
-    HASH_FIND(hh, seen_items, &c, sizeof(char), item);
-    if (item && item->index == index) {
-        HASH_DEL(seen_items, item);
-        free(item);
-    }
-}
-
 int lengthOfLongestSubstring(char *s) {
     if (s[0] == '\0') return 0;
     if (s[1] == '\0') return 1;
 
     int max = 0;
-    int temp = -1;
+    int seen_index = -1;
     int start = 0;
-    int deleted = 0;
 
     for (int i = 0; s[i] != '\0'; i++) {
-        temp = exists(s[i]);
-        if (temp >= 0) {
-            start = temp + 1;
-            while (deleted < start) {
-                remove_item(s[deleted], deleted);
-                deleted++;
-            }
+        seen_index = exists(s[i]);
+        if (seen_index >= start) {
+            start = seen_index + 1;
         }
         add_item(s[i], i);
         max = (i - start + 1) > max ? (i - start + 1) : max;
@@ -68,3 +54,32 @@ int lengthOfLongestSubstring(char *s) {
     HASH_CLEAR(hh, seen_items);
     return max;
 }
+
+
+/*
+
+// a much better solution by https://laurentsv.com/
+
+int max(int a, int b) {
+    return a>b?a:b;
+}
+
+int lengthOfLongestSubstring(char* s) {
+    char* seen[256] = {0};
+    unsigned char c;
+    char* start = s;
+    int res = 0;
+
+    for (; *s; ++s) {
+        c = *s;
+        if (seen[c] && seen[c] >= start) {
+            start = seen[c] + 1;  // Move start just past previous duplicate
+        }
+        seen[c] = s;
+        res = max(res, s - start + 1);
+    }
+
+    return res;
+}
+
+*/
